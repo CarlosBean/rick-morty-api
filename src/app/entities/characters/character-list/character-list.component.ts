65775e548450ca6dd3b0c0a72fd1bb2ec3c41f0c
carlos benavides <1992cab@gmail.com>
2023-01-23 16:38:19 -0500

    chore: added character list component

---
+++ src/app/entities/characters/character-list/character-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-character-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './character-list.component.html',
+  styleUrls: ['./character-list.component.scss']
+})
+export class CharacterListComponent {
+
+}
