fe59413504fd89f3bdc94468ccd1436e6e395004
carlos benavides <1992cab@gmail.com>
2023-01-23 16:37:27 -0500

    chore: added character model

--- src/app/entities/characters/characters.service.ts
+++ src/app/entities/characters/characters.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Character } from './character.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class CharactersService {
   uri = `${environment.API_URL}/`;
   constructor(private http: HttpClient) { }
 
-  getAllCharacters(): Observable<any> {
-    return this.http.get(`${this.uri}/character`);
+  getAllCharacters(): Observable<Character[]> {
+    return this.http.get<Character[]>(`${this.uri}/character`);
   }
 }
