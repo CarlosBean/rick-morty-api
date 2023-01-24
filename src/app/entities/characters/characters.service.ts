0f279d1341c6da5fe6fc6112b7c89ab2c90ae08b
carlos benavides <1992cab@gmail.com>
2023-01-23 22:25:48 -0500

    chore: added loading subject on characters service

--- src/app/entities/characters/characters.service.ts
+++ src/app/entities/characters/characters.service.ts
@@ -1,21 +1,30 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable, tap } from 'rxjs';
+import { BehaviorSubject, finalize, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Character } from './character.model';
+
+export interface ResponseApi {
+  info?: any,
+  results: Character[]
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharactersService {
   resourceUrl = `${environment.API_URL}/character`;
+  loading$ = new BehaviorSubject<boolean>(false);
+
   constructor(private http: HttpClient) { }
 
-  getAllCharacters(page: number): Observable<Character[]> {
+  getAllCharacters(page: number): Observable<ResponseApi> {
     const options: HttpParams = new HttpParams();
     options.append('page', page);
 
-    return this.http.get<Character[]>(this.resourceUrl, { params: options })
-      .pipe(map((res: any) => res.results));
+    this.loading$.next(true);
+
+    return this.http.get<ResponseApi>(this.resourceUrl, { params: options })
+      .pipe(finalize(() => this.loading$.next(false)));
   }
 }
