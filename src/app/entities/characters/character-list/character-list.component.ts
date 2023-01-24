f6cf38b88f346ef4dd55774eb41166467c076e49
carlos benavides <1992cab@gmail.com>
2023-01-23 20:10:41 -0500

    chore: apply service in character list

--- src/app/entities/characters/character-list/character-list.component.ts
+++ src/app/entities/characters/character-list/character-list.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CharactersService } from '../characters.service';
+import { Character } from '../character.model';
+import { Observable } from 'rxjs';
+import { CharacterCardComponent } from '../character-card/character-card.component';
 
 @Component({
   selector: 'app-character-list',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './character-list.component.html',
-  styleUrls: ['./character-list.component.scss']
+  imports: [
+    CommonModule, 
+    CharacterCardComponent
+  ],
+  templateUrl: './character-list.component.html'
 })
 export class CharacterListComponent {
+ constructor(private characters: CharactersService) {}
+ 
+ characters$: Observable<Character[]> = this.characters.getAllCharacters(1);
 
+ trackByFn(index: number, name: Character): number {
+   return name.id;
+ }
 }
