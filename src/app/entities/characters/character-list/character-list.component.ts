addcc50b7fde81e72f9c24fdd8179a5f1b07058e
carlos benavides <1992cab@gmail.com>
2023-01-23 22:27:11 -0500

    chore: apply skeleton cards on loading

--- src/app/entities/characters/character-list/character-list.component.ts
+++ src/app/entities/characters/character-list/character-list.component.ts
@@ -1,23 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CharactersService } from '../characters.service';
+import { CharactersService, ResponseApi } from '../characters.service';
 import { Character } from '../character.model';
 import { Observable } from 'rxjs';
 import { CharacterCardComponent } from '../character-card/character-card.component';
+import { SkeletonCardComponent } from '../character-card/skeleton-card.component';
 
 @Component({
   selector: 'app-character-list',
   standalone: true,
   imports: [
     CommonModule, 
-    CharacterCardComponent
+    CharacterCardComponent,
+    SkeletonCardComponent
   ],
   templateUrl: './character-list.component.html'
 })
 export class CharacterListComponent {
  constructor(private characters: CharactersService) {}
+
+ skeletonItems = new Array(8);
+ loading$ = this.characters.loading$.asObservable();
  
- characters$: Observable<Character[]> = this.characters.getAllCharacters(1);
+ characters$: Observable<ResponseApi> = 
+  this.characters.getAllCharacters(1).pipe();
 
  trackByFn(index: number, name: Character): number {
    return name.id;
