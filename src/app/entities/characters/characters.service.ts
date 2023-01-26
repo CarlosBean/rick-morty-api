a2b9ad92b9d3c07aba0cc45916c171433dd9fdd2
carlos benavides <1992cab@gmail.com>
2023-01-25 21:20:27 -0500

    chore: added get character by id service

--- src/app/entities/characters/characters.service.ts
+++ src/app/entities/characters/characters.service.ts
@@ -1,30 +1,38 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, finalize, Observable } from 'rxjs';
+import { BehaviorSubject, finalize, map, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Character } from './character.model';
 
-export interface ResponseApi {
-  info?: any,
-  results: Character[]
+export interface PageResponse {
+  info?: any;
+  results: Character[];
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CharactersService {
   resourceUrl = `${environment.API_URL}/character`;
   loading$ = new BehaviorSubject<boolean>(false);
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getAllCharacters(page: number): Observable<ResponseApi> {
-    const options: HttpParams = new HttpParams();
-    options.append('page', page);
+  getAllCharacters(page: number): Observable<PageResponse> {
+    let params: HttpParams = new HttpParams().append('page', page);
 
     this.loading$.next(true);
 
-    return this.http.get<ResponseApi>(this.resourceUrl, { params: options })
+    return this.http
+      .get<PageResponse>(this.resourceUrl, { params: params })
+      .pipe(finalize(() => this.loading$.next(false)));
+  }
+
+  getCharacterById(id: number): Observable<Character> {
+    this.loading$.next(true);
+
+    return this.http
+      .get<Character>(`${this.resourceUrl}/${id}`)
       .pipe(finalize(() => this.loading$.next(false)));
   }
 }
