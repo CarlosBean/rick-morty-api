0a6a758286c79f692d7047ce6684fb3dba9d1bbe
carlos benavides <1992cab@gmail.com>
2023-01-25 21:21:04 -0500

    chore: applied character by id service

--- src/app/entities/characters/character-detail/character-detail.component.ts
+++ src/app/entities/characters/character-detail/character-detail.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CharactersService } from '../characters.service';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Character } from '../character.model';
 
 @Component({
   selector: 'app-character-detail',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './character-detail.component.html'
+  templateUrl: './character-detail.component.html',
 })
 export class CharacterDetailComponent {
+  character$!: Observable<Character>;
+  loading$ = this.charactersService.loading$.asObservable();
 
+  constructor(
+    private charactersService: CharactersService,
+    private route: ActivatedRoute
+  ) {
+    const params = this.route.snapshot.params;
+    const characterId = params['id'];
+    this.character$ = this.charactersService.getCharacterById(characterId);
+  }
 }
