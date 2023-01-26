3a657220a9a81e53d982eed94f47852554e8c854
carlos benavides <1992cab@gmail.com>
2023-01-25 21:17:45 -0500

    format: applied prettier format

--- src/app/entities/characters/character.model.ts
+++ src/app/entities/characters/character.model.ts
@@ -1,22 +1,22 @@
 export type Status = 'Alive' | 'Dead' | 'unknown';
-export type Gender = 'Female' | 'Male' | 'Genderless' | 'unknown'
+export type Gender = 'Female' | 'Male' | 'Genderless' | 'unknown';
 
 export interface Character {
-  id:        number;
-  name?:     string;
-  status:    Status;
-  species?:  string;
-  type?:     string;
-  gender:    Gender;
-  origin?:   Location;
+  id: number;
+  name?: string;
+  status: Status;
+  species?: string;
+  type?: string;
+  gender: Gender;
+  origin?: Location;
   location?: Location;
-  image?:    string;
-  episode?:  string[];
-  url?:      string;
-  created?:  Date;
+  image?: string;
+  episode?: string[];
+  url?: string;
+  created?: Date;
 }
 
 export interface Location {
   name?: string;
-  url?:  string;
+  url?: string;
 }
