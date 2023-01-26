43af175488664e6e083a4e6df1a258319a010806
carlos benavides <1992cab@gmail.com>
2023-01-25 21:21:41 -0500

    chore: added navigate to character

--- src/app/entities/characters/character-list/character-list.component.ts
+++ src/app/entities/characters/character-list/character-list.component.ts
@@ -1,31 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CharactersService, ResponseApi } from '../characters.service';
+import { CharactersService, PageResponse } from '../characters.service';
 import { Character } from '../character.model';
 import { Observable } from 'rxjs';
 import { CharacterCardComponent } from '../character-card/character-card.component';
 import { SkeletonCardComponent } from '../character-card/skeleton-card.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-character-list',
   standalone: true,
-  imports: [
-    CommonModule, 
-    CharacterCardComponent,
-    SkeletonCardComponent
-  ],
-  templateUrl: './character-list.component.html'
+  imports: [CommonModule, CharacterCardComponent, SkeletonCardComponent],
+  templateUrl: './character-list.component.html',
 })
 export class CharacterListComponent {
- constructor(private characters: CharactersService) {}
+  constructor(private characters: CharactersService, private router: Router) {}
 
- skeletonItems = new Array(8);
- loading$ = this.characters.loading$.asObservable();
- 
- characters$: Observable<ResponseApi> = 
-  this.characters.getAllCharacters(1).pipe();
+  skeletonItems = new Array(8);
+  loading$ = this.characters.loading$.asObservable();
 
- trackByFn(index: number, name: Character): number {
-   return name.id;
- }
+  characters$: Observable<PageResponse> = this.characters.getAllCharacters(2);
+
+  trackByFn(index: number, name: Character): number {
+    return name.id;
+  }
+
+  navigateTo(index: number): void {
+    this.router.navigate(['characters', index]);
+  }
 }
