babec7d6e6d6c4931ed12dbb76f097cbbd2b5cdd
carlos benavides <1992cab@gmail.com>
2023-01-26 22:09:24 -0500

    chore: applied pagination on character list

--- src/app/entities/characters/character-list/character-list.component.ts
+++ src/app/entities/characters/character-list/character-list.component.ts
@@ -7,20 +7,36 @@ import { CharacterCardComponent } from '../character-card/character-card.compone
 import { SkeletonCardComponent } from '../character-card/skeleton-card.component';
 import { Router } from '@angular/router';
 import { PageResponse } from 'src/app/core/model/page-response.model';
+import { PaginatorComponent } from 'src/app/core/components/paginator/paginator.component';
 
 @Component({
   selector: 'app-character-list',
   standalone: true,
-  imports: [CommonModule, CharacterCardComponent, SkeletonCardComponent],
+  imports: [
+    CommonModule,
+    CharacterCardComponent,
+    SkeletonCardComponent,
+    PaginatorComponent,
+  ],
   templateUrl: './character-list.component.html',
 })
 export class CharacterListComponent {
-  constructor(private characters: CharactersService, private router: Router) {}
+  skeletonItems!: object[];
+  currentPage!: number;
 
-  skeletonItems = new Array(8);
-  loading$ = this.characters.loading$.asObservable();
+  loading$!: Observable<boolean>;
+  characters$!: Observable<PageResponse>;
 
-  characters$: Observable<PageResponse> = this.characters.getAllCharacters(2);
+  constructor(private characters: CharactersService, private router: Router) {
+    this.skeletonItems = new Array(8);
+    this.loading$ = this.characters.loading$.asObservable();
+    this.getAllCharacters(1);
+  }
+
+  getAllCharacters(page: number): void {
+    this.currentPage = page;
+    this.characters$ = this.characters.getAllCharacters(page);
+  }
 
   trackByFn(index: number, name: Character): number {
     return name.id;
