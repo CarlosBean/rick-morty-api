ae6428cf231328b883252a99bd0dbd2ac4691d8c
carlos benavides <1992cab@gmail.com>
2023-01-31 09:10:36 -0500

    chore: adjust styles on skeleton card

--- src/app/entities/characters/character-card/skeleton-card.component.ts
+++ src/app/entities/characters/character-card/skeleton-card.component.ts
@@ -7,24 +7,24 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   imports: [CommonModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <div class="flex h-48 animate-pulse rounded-md bg-slate-200 p-2">
-      <div class="h-36 w-32 rounded-xl bg-slate-400"></div>
+    <div class="flex h-48 animate-pulse rounded-md bg-white p-2">
+      <div class="h-auto w-32 rounded-xl bg-slate-200"></div>
 
       <div class="flex-1 p-3 pb-0">
         <div class="space-y-5">
           <div class="space-y-2">
-            <div class="h-6 w-40 rounded-sm bg-slate-400"></div>
-            <div class="h-5 w-20 rounded-sm bg-slate-400"></div>
+            <div class="h-6 w-40 rounded-sm bg-slate-200"></div>
+            <div class="h-5 w-20 rounded-sm bg-slate-200"></div>
           </div>
 
           <div class="space-y-2">
-            <div class="h-3 w-48 rounded-sm bg-slate-400"></div>
-            <div class="h-3 rounded-sm bg-slate-400"></div>
+            <div class="h-3 w-48 rounded-sm bg-slate-200"></div>
+            <div class="h-3 rounded-sm bg-slate-200"></div>
           </div>
 
           <div class="space-y-2">
-            <div class="h-3 w-28 rounded-sm bg-slate-400"></div>
-            <div class="h-3 rounded-sm bg-slate-400"></div>
+            <div class="h-3 w-28 rounded-sm bg-slate-200"></div>
+            <div class="h-3 rounded-sm bg-slate-200"></div>
           </div>
         </div>
       </div>
