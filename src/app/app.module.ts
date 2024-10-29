2c628f1634db7db61945da50d5c61f229b74c2a8
carlos benavides <1992cab@gmail.com>
2024-10-28 19:21:10 -0500

    chore: update to angular v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { SearchBarComponent } from './core/components/search-bar/search-bar.component';
 
-@NgModule({
-  declarations: [AppComponent, NavbarComponent, SearchBarComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent, NavbarComponent, SearchBarComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule, AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
